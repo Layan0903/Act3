15ca16e9866c7e8158a00e4c007dc43001280ea1
Layan0903 <you@example.com>
2022-01-06 01:07:29 +0800

    adding contents to my pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    GalleryComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
